f931eb44375af9d1b301980f8dba2b678651943b
高渝昕 <s1081673@gm.pu.edu.tw>
2024-08-26 01:17:45 +0800

    add file

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextMessage, MessageEvent, TextSendMessage
+import os
+import openai
+import tempfile
+import datetime
+import time
+import string
+# from fine_tune import GPT_response
+
+app = Flask(__name__, template_folder='templates')
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+# OPENAI API Key初始化設定
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+
+@app.route("/")
+def index():
+    return render_template("./index.html")
+
+@app.route("/heroku_wake_up")
+def heroku_wake_up():
+    return "Hey!Wake Up!!"
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+
+# # 處理訊息
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     msg = event.message.text
+#     GPT_answer = GPT_response(msg)  # 在這裡呼叫 GPT_response 函數處理用戶的訊息
+#     print(GPT_answer)
+#     line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
